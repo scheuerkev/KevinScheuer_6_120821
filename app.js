1acfa2b1d6bdefbb67dfe4df084a6632280e6c36
Scheuer Kévin <scheuer.kevin@gmail.com>
2021-08-17 16:36:03 +0200

    Setting Express app and dotenv configuration

---
+++ app.js
@@ -0,0 +1,42 @@
+//DotEnv requirement
+require('dotenv').config();
+
+//OpenAPI 3.0 Specifications and requirements
+
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const openAPIOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Piiquante API",
+            version: "1.0.0",
+            description: "Piiquante is a private Express RESTful API to provide HTTP services to Hot Takes web app"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const openAPISpecs = swaggerJSDoc(openAPIOptions);
+const app = express();
+mongoose.connect(process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(()=> console.log('Data base connection ok...'))
+    .catch(()=> console.log('Data base connection failure'));
+
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
+
+module.exports = app;
