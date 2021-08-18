fc96c7b7e8d1b634888b42f981b69345f916d000
Scheuer Kévin <scheuer.kevin@gmail.com>
2021-08-18 20:20:35 +0200

    Setting up express-rate-limiter to prevent bruteforce

--- app.js
+++ app.js
@@ -2,10 +2,8 @@
 require('dotenv').config();
 
 //OpenAPI 3.0 Specifications and requirements
-
 const swaggerUI = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
-
 const openAPIOptions = {
     definition: {
         openapi: "3.0.0",
@@ -23,10 +21,18 @@ const openAPIOptions = {
     apis: ["./routes/*.js"],
 };
 
+//Express rate-limit to constrain number of requests
+const rateLimit = require('express-rate-limit');
+const apiLimiter = rateLimit({
+    windowsMs: 15 * 60 * 1000, //15 minutes
+    max: 100
+});
+
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const userRoutes = require('./routes/user.js');
+const sauceRoutes  = require('./routes/sauce.js');
 
 const openAPISpecs = swaggerJSDoc(openAPIOptions);
 const app = express();
@@ -38,16 +44,19 @@ mongoose.connect(process.env.MONGO_URI,
     .then(()=> console.log('Data base connection ok...'))
     .catch(()=> console.log('Data base connection failure'));
 
-
+//CORS Policy
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/api/', apiLimiter);
 app.use(express.json());
 app.use(helmet());
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
 
 module.exports = app;
